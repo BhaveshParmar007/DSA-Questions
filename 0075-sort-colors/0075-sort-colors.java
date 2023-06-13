655ff2818bde3b56884222836db77c359432bcdf
Bhavesh Parmar <bhavesh20csu215@ncuindia.edu>
2023-06-13 21:33:21 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (71.80%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        
+        while(mid<=high){
+            if(nums[mid]==0){
+               // swap(nums,low,mid);
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+              //  swap(nums,mid,high);
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+               
+                high--;
+            }
+        }
+        
+        }
+    //     public static void swap(int i ,int j){
+    //         int temp = i;
+    //         i = j;
+    //         j = temp;
+    // }
+}
