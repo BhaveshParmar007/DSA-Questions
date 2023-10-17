b421797f18fff874ece8c5a5dd1fa9e18e662711
Bhavesh Parmar <bhavesh20csu215@ncuindia.edu>
2023-10-17 17:11:30 +0530

    Added solution - LeetHub

--- Peak element - GFG/peak-element.java
+++ Peak element - GFG/peak-element.java
@@ -49,21 +49,27 @@ class Solution
 	// Function to find the peak element
 	// arr[]: input array
 	// n: size of array a[]
-public int peakElement(int[] arr, int n) {
-    int start = 0;
-    int end = n - 1;
-
-    while (start < end) {
-        int mid = start + (end - start) / 2;
-
-        if (arr[mid] > arr[mid + 1]) {
-            end = mid;
-        } else {
-            start = mid + 1;
-        }
+	public int peakElement(int[] arr,int n)
+    {
+       //add code here.
+       int ans = 0;
+       if(n==1){
+           return 0;
+       }
+       
+       if(arr[n-1]>arr[n-2]){
+           return n-1;
+       }
+       
+       for(int i = 1; i<n-1 ; i++){
+           if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1]){
+               ans = i;
+           }
+           
+           
+       }
+       return ans;
+       
+       
     }
-
-    return start;
-}
-
 }
